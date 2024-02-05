8245565a5725b73bfdd671a4934117d628b14597
Shakti7746 <shaktimankushwaha14@gmail.com>
2024-02-05 13:25:57 +0530

    some changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const {currentUser} = useContext(AuthContext)
 
   return (
     <div className='navbar'>
-      <span className="logo">Chat App</span>
+      <span className="logo">Hola Chat</span>
       <div className="user">
         <img src={currentUser.photoURL} alt="" />
         <span>{currentUser.displayName}</span>
